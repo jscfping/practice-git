d4dba8b59a4a21cc9e4d7ee39818fc333b599731
jscfping <jscfping@gmail.com>
2019-12-15 21:56:34 +0800

    a test node JS file to run from start

---
+++ app.js
@@ -0,0 +1,33 @@
+var express               = require("express"),
+    app                   = express(),
+    bodyParser            = require("body-parser"),
+    mongoose              = require("mongoose"),
+
+
+//mongoose setup
+mongoose.set("useUnifiedTopology", true);
+mongoose.connect("mongodb://localhost/test_db", {useNewUrlParser: true});  //use for mongo's use sth
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+	console.log("mongoose........connected");    // we're connected!
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+
+
+
+
+
+app.get("/", function(req, res){
+    res.send("a test homepage");
+});
+
+
+
+
+app.listen(3000, function(){
+   console.log("Server is running..");
+});
